ead56f886b96966c2fd85a046c8d3e48168ea3eb
VASUKI <vasukinfc@gmail.com>
2026-03-15 16:23:49 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase, ref, get, set, update } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  databaseURL: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
-  measurementId: "..."
+  apiKey: "AIzaSyBGHfGSGZsdiIrAUmJxPkXJmCLuAk4ctYE",
+  authDomain: "vasuki-nfc-d93db.firebaseapp.com",
+  databaseURL: "https://vasuki-nfc-d93db-default-rtdb.firebaseio.com",
+  projectId: "vasuki-nfc-d93db",
+  storageBucket: "vasuki-nfc-d93db.firebasestorage.app",
+  messagingSenderId: "159152694151",
+  appId: "1:159152694151:web:e5b306ac03edb60f8de922",
+  measurementId: "G-YR12LK0NB4"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-export { db, ref, get, set, update };
+export { db, ref, set };
