1b6ed9d0fa981b87b0660eb4e9519dbfc66a4f99
VASUKI <vasukinfc@gmail.com>
2026-03-23 20:41:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getDatabase, ref, set, get, update } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
 import { getAuth, signInWithEmailAndPassword, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getStorage, ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBg-MhiTCTG56NSSMZNwNVZ8CBOkD1YQ3c",
+ apiKey: "AIzaSyBg-MhiTCTG56NSSMZNwNVZ8CBOkD1YQ3c",
   authDomain: "vasuki-60ed9.firebaseapp.com",
   databaseURL: "https://vasuki-60ed9-default-rtdb.firebaseio.com",
   projectId: "vasuki-60ed9",
@@ -11,9 +12,15 @@ const firebaseConfig = {
   messagingSenderId: "233551822151",
   appId: "1:233551822151:web:9f43ad1e97fe6270c26062"
 };
+
 const app = initializeApp(firebaseConfig);
 
 const db = getDatabase(app);
 const auth = getAuth(app);
+const storage = getStorage(app);
 
-export { db, ref, set, get, update, auth, signInWithEmailAndPassword, onAuthStateChanged, signOut };
+export {
+  db, ref, set, get, update,
+  auth, signInWithEmailAndPassword, onAuthStateChanged, signOut,
+  storage, sRef, uploadBytes, getDownloadURL
+};
