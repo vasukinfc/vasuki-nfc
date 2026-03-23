1b4dda9d5b28804b72a91eeff182bf7b366e084e
VASUKI <vasukinfc@gmail.com>
2026-03-23 17:16:15 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getDatabase, ref, set, get, update } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apiKey: "AIzaSyBGHfGSGZsdiIrAUmJxPkXJmCLuAk4ctYE",
-authDomain: "vasuki-nfc-d93db.firebaseapp.com",
-databaseURL: "https://vasuki-nfc-d93db-default-rtdb.firebaseio.com",
-projectId: "vasuki-nfc-d93db",
-storageBucket: "vasuki-nfc-d93db.appspot.com",
-messagingSenderId: "159152694151",
-appId: "1:159152694151:web:e5b306ac03edb60f8de922"
+  apiKey: "AIzaSyBg-MhiTCTG56NSSMZNwNVZ8CBOkD1YQ3c",
+  authDomain: "vasuki-60ed9.firebaseapp.com",
+  databaseURL: "https://vasuki-60ed9-default-rtdb.firebaseio.com",
+  projectId: "vasuki-60ed9",
+  storageBucket: "vasuki-60ed9.firebasestorage.app",
+  messagingSenderId: "233551822151",
+  appId: "1:233551822151:web:9f43ad1e97fe6270c26062",
+  measurementId: "G-FKD654MJF5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-export { db, ref, set, get, update };
+const analytics = getAnalytics(app);
